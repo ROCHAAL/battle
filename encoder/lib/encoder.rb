66310e9f5ce49e1b0d23c327eecff812c2ac456d
Patricia <prmonday@gmail.com>
2021-02-04 17:57:02 +0000

    testing

--- encoder/lib/encoder.rb
+++ encoder/lib/encoder.rb
@@ -1,5 +1,12 @@
 class Encoder
+
+
   def duplicated_letters
     0
   end
+
+def get_length(input_string)
+3
+end
+
 end
--- encoder/spec/encoder_spec.rb
+++ encoder/spec/encoder_spec.rb
@@ -5,5 +5,14 @@ it ' counts how many times the same letter are de-duplicated  in a string.'do
   encoder = Encoder.new
   encoder.duplicated_letters
 expect(encoder.duplicated_letters).to eq(0)
+
+
+  end
+  it 'gets the lenght of a string' do
+    encoder = Encoder.new
+    result_1 =encoder.get_length("aaa")
+    result_2 = encoder.get_length('bbbbb')
+    expect(result_1).to eq(3)
+    expect(result_2).to eq(5)
   end
 end
